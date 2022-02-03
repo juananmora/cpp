4943e4894edd5c4ce7fbd3b24abbd56d5bbd4bc3
Jon <52193917+juananmora@users.noreply.github.com>
2022-02-03 20:30:58 +0100

    Update hello.cc

--- hello.cc
+++ hello.cc
@@ -5,7 +5,7 @@ int main()
 {
     int firstNumber, secondNumber, sumOfTwoNumbers;
     
-    cout << "Enter two integers: ";
+    cout << "Enter two enteros: ";
     cin >> firstNumber >> secondNumber;
 
     // sum of two numbers in stored in variable sumOfTwoNumbers
